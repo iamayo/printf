b88ad6869d0cc73d07447c170b691947970929b4
Olaitan Adedokun <kenney9224@gmail.com>
2022-04-18 21:13:03 +0100

    An updated header file

--- main.h
+++ main.h
@@ -5,20 +5,31 @@
 #include <stdarg.h>
 #include <stdio.h>
 
-int _putchar(int ch);
-int _printf(const char *format, ...);
-void _printString(va_list myList);
-void _printFloat(va_list myList);
-void _printInt(va_list myList);
-void _printChar(va_list myList);
+/**
+ * struct convert - A convert struct
+ * @type: First member
+ * @func: Second member
+ * Description: Define a new type struct } converter
+ */
+struct convert
+{
+	char *type;
+	int (*func)();
+};
 
 /**
  * converter - Typedef for struct convert
  */
-typedef struct convert
-{
-	char *type;
-	void (*func)();
-} converter;
+typedef struct convert converter;
+
+int _putchar(int ch);
+int _printf(const char *format, ...);
+int _printString(va_list myList);
+int _printFloat(va_list myList);
+int _printInt(va_list myList);
+int _printChar(va_list myList);
+int _printCent(va_list myList);
+int _print_specifier(const char *format, converter myarray[], va_list my_list);
+
 
 #endif
